bb0e69244c668669cb4113ee3d0c9c4f30db1095
Yuriy Izbash <izbash91@gmail.com>
2025-11-11 11:24:24 +0100

    Changed BackgroundIimage

--- Magic8Ball/ContentView.swift
+++ Magic8Ball/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = "Ask"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(.ball)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 400, height: 400)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .overlay(
+                Text(text)
+                    .font(Font.custom("Magic School One", size: 24))
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
+            )
+            .shadow(color: Color.blue.opacity(0.5), radius: 20, x: 0, y: 20)
+            .background(Color.black.frame(width: 200, height: 200))
+            .background(Image(.background))
+            .padding()
+        
+        Text("Shake to see the answer!")
+            .foregroundColor(.white)
+            .font(Font.custom("Magic School One", size: 42))
+            .shadow(color: .red, radius: 10)
+            .padding()
     }
 }
 
